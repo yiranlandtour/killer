1a2eff917918e0f99b98396a9a53796de6f5c65b
yiranlandtour <yiranlandtour@163.com>
2013-02-25 16:17:36 +0800

    firstRun
    1 、界面不漂亮
    2、无软件信息
    3、中间页面的bug
    4、没有关闭view

--- gen/com/example/killer/R.java
+++ gen/com/example/killer/R.java
@@ -25,11 +25,11 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f080008;
-        public static final int button1=0x7f080007;
-        public static final int checkBox1=0x7f080006;
-        public static final int editText1=0x7f080003;
+        public static final int button1=0x7f080006;
+        public static final int checkBox1=0x7f080005;
+        public static final int editText1=0x7f080007;
         public static final int editText2=0x7f080004;
-        public static final int editText3=0x7f080005;
+        public static final int editText3=0x7f080003;
         public static final int textView1=0x7f080000;
         public static final int textView2=0x7f080001;
         public static final int textView3=0x7f080002;
@@ -37,7 +37,8 @@ public final class R {
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int choosen=0x7f030001;
-        public static final int result=0x7f030002;
+        public static final int next=0x7f030002;
+        public static final int result=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- src/com/example/killer/MainActivity.java
+++ src/com/example/killer/MainActivity.java
@@ -1,29 +1,47 @@
 package com.example.killer;
 
+import java.util.Random;
+
 import android.os.Bundle;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
 import android.widget.TextView;
 
 @SuppressLint("NewApi") public class MainActivity extends Activity {
 
-    public String tempString;
-    public int killerNum;
-    public int policeNum;
-    public int peaNum;
-    
+    public int godExist = 0;
+	public String tempString;
+    public int killerNum =0;
+    public int policeNum = 0;
+    public int peaNum = 0;
+    public int length = 0;
+    public int all[] = new int[200];
+    public int whichOne = 0;
 	
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
         
-       
-        
+       /*
+        //获取CheckBox实例
+        CheckBox cb = (CheckBox)this.findViewById(R.id.checkBox1);
+        //绑定监听器
+        cb.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+                    
+                   
+                    public void onCheckedChanged(CompoundButton arg0, boolean arg1) {
+                        godExist = 1;
+                    }
+                });
+                */
         setContentView(R.layout.activity_main);
   
     }
@@ -33,7 +51,8 @@ import android.widget.TextView;
     
 public void start(View view)
 {
-	 Button myButton = (Button) findViewById(R.id.button1);
+	int i,j; 
+	Button myButton = (Button) findViewById(R.id.button1);
 	 
 	 /* to get the basic number of role  */
      EditText editText1 = (EditText) findViewById(R.id.editText1);
@@ -48,19 +67,115 @@ public void start(View view)
      tempString = editText3.getText().toString();
      peaNum = Integer.parseInt(tempString);
 	
+
+     CheckBox cb = (CheckBox)this.findViewById(R.id.checkBox1);
+     if(cb.isChecked()) godExist = 1;
      
      /* a wise rule to create the order*/
+     length = peaNum + killerNum;
+     length = policeNum + length;
      
+     for(i = 0; i < length; i++)
+     {
+    	 for(j = 0; j < peaNum; j++,i++)
+    	 {
+    		 all[i] = 1;
+    	 }
+    	 for(j = 0; j < killerNum; j++,i++)
+    	 {
+    		 all[i] = 2;
+    	 }
+    	 for(j = 0; j < policeNum; j++,i++)
+    	 {
+    		 all[i] = 3;
+    	 }
+    	 
+     }
      
+     /*if god exist*/
+     if(1 == godExist)
+     {
+    	 length = length+1;
+    	 all[length-1] = 0;   	 
+     }
      
+     int temp;
+     //have a sequence
+     for(j = 0; j < 10;j++)
+     for(i = 0; i < length; i++)
+     {
+    	 Random random1 = new Random();
+         int ramd = random1.nextInt(length);
+
+		temp = all[ramd];
+         all[ramd] = all[i];
+         all[i] = temp;
+
+     }
+     
+
+    /* try to init choosen */ 
 	setContentView(R.layout.choosen);
+	String text = String.valueOf(whichOne+1);
+	TextView textview1 = (TextView)findViewById(R.id.textView1);
+    textview1.setText(text);
+    text = findwhich(all[whichOne]); 
+    TextView textview2 = (TextView)findViewById(R.id.textView2);
+    textview2.setText(text);
+    
+    
+    
 }
 
+public String findwhich(int a)
+{
+	String text = "hello";
+	if(0 == a)
+		text="法官";
+	if(2 == a)
+		text="杀手";
+	if(3 == a)
+		text="警察";
+	if(1 == a)
+		text="平民";
+	whichOne++;
+	return text;
+}
 public void choose(View view)
 {
 	
+	if(whichOne > length)
+		result();
+	setContentView(R.layout.choosen);
+	String text = String.valueOf(whichOne+1);
+	TextView textview1 = (TextView)findViewById(R.id.textView1);
+    textview1.setText(text);
+    text = findwhich(all[whichOne]); 
+    TextView textview2 = (TextView)findViewById(R.id.textView2);
+    textview2.setText(text);
+    
+    
 }
-
+public void next(View view)
+{
+	
+	setContentView(R.layout.next);
+}
+/* show the result */
+public void result()
+{
+	setContentView(R.layout.result);
+	String text="结果如下\n",temp;
+	int i;
+	for(i = 0; i < length;i++)
+	{
+	temp = String.valueOf(i+1) + findwhich(all[i]);
+	text = text + temp;
+	text = text + "\n";
+	}
+	TextView textview1 = (TextView)findViewById(R.id.textView1);
+    textview1.setText(text);
+}
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
